704e39e768d783abc0c0d9497f26b1f3993093e1
Prasanthi90 <132976922+Prasanthi90@users.noreply.github.com>
2023-07-12 09:38:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 provider "aws" {
   region = "ap-south-1"
-  access_key = "AKIA4QUKASHDSEKLAZKU"
-  secret_key = "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"
+  access_key = "AKIATY4QYXIT2O3NYGVM"
+  secret_key = kHw2ii8awt8q9fRD8u7c8UvwkwgVN59lRlPU4gOf""
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0e6329e222e662a52"
+  ami           = "ami-06b79cf2aee0d5c92"
   instance_type = var.instance_type
   tags= {
-    Name = "raham"
+    Name = "prasanthi"
   }
 }
 
